25ff4851973016c2e282e5d0639cda697032af54
pearlwang1106 <610999886@qq.com>
2018-10-07 20:49:04 +0800

    add: test

--- src/Math/fibonacci/index.js
+++ src/Math/fibonacci/index.js
@@ -5,16 +5,18 @@
  * 
  * @return {Number} nth fibonacci number
  */
-export default function fibonacci(n) {
+function fibonacci(n) {
     if (n < 0) {
         return 0;
     }
 
-    let a = -1;
+    let a = 0;
     let b = 1;
-    while (n-- >= 1) {
+    while (n-- > 1) {
         b += a;
         a = b - a;
     }
     return b;
 }
+
+module.exports = fibonacci;
---
+++ src/Math/fibonacci/index.test.js
@@ -0,0 +1,14 @@
+const fibonacci = require('./index');
+
+test('Math.fibonacci', () => {
+    expect(fibonacci(1)).toBe(1);
+    expect(fibonacci(2)).toBe(1);
+    expect(fibonacci(3)).toBe(2);
+    expect(fibonacci(4)).toBe(3);
+    expect(fibonacci(5)).toBe(5);
+    expect(fibonacci(6)).toBe(8);
+    expect(fibonacci(7)).toBe(13);
+    expect(fibonacci(8)).toBe(21);
+    expect(fibonacci(9)).toBe(34);
+    expect(fibonacci(10)).toBe(55);
+  });
